8ae247cd2492ecb1c34b64c733119c6f61e77386
nmabbasi <nmabbasi@gmail.com>
2026-08-15 18:12:30 +0200

    chore: remove Twitter/X social links and share buttons as requested

--- script.js
+++ script.js
@@ -571,11 +571,7 @@ async function showTutorial(tutorialId, addToHistory = true) {
                     <div class="flex items-center justify-between">
                         <div class="flex items-center space-x-4">
                             <span class="text-sm text-gray-500">Share this tutorial:</span>
-                            <button onclick="shareOnTwitter('${tutorial.title}', '${tutorial.id}')" class="text-gray-900 hover:text-black transition-colors" title="Share on X">
-                                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-                                    <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.744l7.737-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
-                                </svg>
-                            </button>
+
                             <button onclick="shareOnLinkedIn('${tutorial.title}', '${tutorial.id}')" class="text-blue-700 hover:text-blue-900 transition-colors" title="Share on LinkedIn">
                                 <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
                                     <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
@@ -770,13 +766,6 @@ function estimateReadingTime(content) {
     const wordsPerMinute = 200;
     const words = content.split(/\s+/).length;
     return Math.ceil(words / wordsPerMinute);
-}
-
-function shareOnTwitter(title, tutorialId) {
-    const url = `${window.location.origin}${window.location.pathname.replace('index.html', '')}${tutorialId}.html`;
-    const text = `Check out this tutorial: ${title}`;
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(text)}&url=${encodeURIComponent(url)}`;
-    window.open(twitterUrl, '_blank');
 }
 
 function shareOnLinkedIn(title, tutorialId) {
