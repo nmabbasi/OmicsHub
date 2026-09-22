02890cda0e3b2779f04231a5f33784410bef3b53
nmabbasi <nmabbasi@gmail.com>
2026-09-22 21:00:45 +0200

    chore: clean up repo root hygiene by moving build scripts and logs to hidden scripts folder

--- fix_all_tutorials.py
+++
@@ -1,9 +0,0 @@
-with open("index.html", "r", encoding="utf-8") as file:
-    content = file.read()
-
-new_content = content.replace('href="#all-tutorials"', 'href="tutorials.html"')
-
-if new_content != content:
-    with open("index.html", "w", encoding="utf-8") as file:
-        file.write(new_content)
-    print("Fixed #all-tutorials in index.html")
--- fix_count.py
+++
@@ -1,7 +0,0 @@
-with open("index.html", "r", encoding="utf-8") as f:
-    content = f.read()
-
-content = content.replace('aria-label="57 tutorials">57</span>', 'aria-label="58 tutorials">58</span>')
-
-with open("index.html", "w", encoding="utf-8") as f:
-    f.write(content)
